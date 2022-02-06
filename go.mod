7f120274d4e868e4a3312a9961c242f86647fb01
Harry Bagdi <harrybagdi@gmail.com>
2022-02-06 23:41:51 +0530

    works but no newline

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/hbagdi/hit
 
 go 1.17
+
+require (
+	github.com/alecthomas/participle/v2 v2.0.0-alpha7.0.20211230082035-5a357f57e525
+	github.com/alecthomas/repr v0.0.0-20220113201626-b1b626ac65ae
+)
--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+	"github.com/alecthomas/repr"
+	"log"
+	"net/http"
+	"os"
+)
+
+var hitLexer = lexer.MustSimple([]lexer.SimpleRule{
+	{"comment", `[#][^\n]*`},
+	{"Ident", `[a-zA-Z_]\w*`},
+	{"Path", `/.*`},
+	{"Punct", `[[!@#~$%^&*()+_={}\|:;"'<,>.?/]|]`},
+	{"whitespace", `[\s]+`},
+	{"jump", `[\n]+`},
+	{"Marker", `[\-]{3}`},
+})
+
+type File struct {
+	Requests []*Requests `@@*`
+}
+
+type Requests struct {
+	Id           string `"---" "@"@Ident`
+	Method       string `@Ident?`
+	Path         string `@Path `
+	BodyEncoding string `("~"@Ident)?`
+	Body         string `@!Marker* Marker`
+}
+
+var parser = participle.MustBuild(&File{},
+	participle.Lexer(hitLexer),
+)
 
 func main() {
-	fmt.Println("hit")
+	hitFile := &File{}
+	err := parser.Parse("", os.Stdin, hitFile)
+	if err != nil {
+		panic(err)
+	}
+	repr.Println(hitFile, repr.Indent("  "), repr.OmitEmpty(true))
+	for _, req := range hitFile.Requests {
+		fmt.Println(req.Body)
+		if req.Id == "c1" {
+			req, err := http.NewRequest(req.Method, "http://httpbin.org"+req.Path, nil)
+			if err != nil {
+				log.Println(err)
+			}
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Println(err)
+			}
+			repr.Println(res.StatusCode)
+		}
+	}
 }
