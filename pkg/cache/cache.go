fce89e6b2c374a81f26906a93b003fd5de04cae4
Harry <harrybagdi@gmail.com>
2022-02-19 18:23:53 +0530

    handle empty cache

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -12,29 +12,59 @@ import (
 	"github.com/hbagdi/hit/pkg/parser"
 )
 
+const (
+	cacheDir  = "hit"
+	cacheFile = "cache.json"
+	fileMode  = 0o0600
+)
+
 var cacheFilePath string
 
 func init() {
-	cacheDir, err := os.UserCacheDir()
+	userCacheDir, err := os.UserCacheDir()
 	if err != nil {
 		panic(fmt.Sprintf("failed to find cache directory: %v", err))
 	}
-	cacheFilePath = filepath.Join(cacheDir, cacheFileName)
-	_, err = os.Stat(cacheDir + "/hit")
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			err := os.Mkdir(cacheDir+"/hit", os.ModePerm)
-			if err != nil {
-				panic(err)
-			} else {
-				return
-			}
-		}
+	if err = ensureDir(filepath.Join(userCacheDir, cacheDir),
+		os.ModePerm); err != nil {
 		panic(err)
 	}
+	if err = ensureFile(filepath.Join(userCacheDir,
+		cacheDir, cacheFile), fileMode); err != nil {
+		panic(err)
+	}
+	cacheFilePath = filepath.Join(userCacheDir, cacheDir, cacheFile)
 }
 
-const cacheFileName = "hit/cache.json"
+func ensureFile(path string, perm os.FileMode) error {
+	_, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err := os.WriteFile(path, []byte("{}"), perm)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return err
+	}
+	return nil
+}
+
+func ensureDir(path string, perm os.FileMode) error {
+	_, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(path, perm)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return err
+	}
+	return nil
+}
 
 func Load() (map[string]interface{}, error) {
 	content, err := ioutil.ReadFile(cacheFilePath)
@@ -80,5 +110,5 @@ func Save(req parser.Request, resp *http.Response) error {
 		return err
 	}
 
-	return ioutil.WriteFile(cacheFilePath, f, 0600)
+	return ioutil.WriteFile(cacheFilePath, f, fileMode)
 }
