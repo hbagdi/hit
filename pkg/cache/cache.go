901217059574ea21120b152ab289c8c4a6718c48
Harry <harrybagdi@gmail.com>
2022-02-19 13:54:08 +0530

    move cache file under user cache dir

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -2,16 +2,50 @@ package cache
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/hbagdi/hit/pkg/parser"
 )
 
-func Load() (map[string]interface{}, error) {
-	content, err := ioutil.ReadFile(".hit.cache")
+var cacheFilePath string
+
+func init() {
+	cacheDir, err := os.UserCacheDir()
 	if err != nil {
+		panic(fmt.Sprintf("failed to find cache directory: %v", err))
+	}
+	cacheFilePath = filepath.Join(cacheDir, cacheFileName)
+	_, err = os.Stat(cacheDir + "/hit")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(cacheDir+"/hit", os.ModePerm)
+			if err != nil {
+				panic(err)
+			} else {
+				return
+			}
+		}
+		panic(err)
+	}
+}
+
+const cacheFileName = "hit/cache.json"
+
+func Load() (map[string]interface{}, error) {
+	content, err := ioutil.ReadFile(cacheFilePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return map[string]interface{}{}, nil
+		}
 		return nil, err
+	}
+	if len(content) == 0 {
+		return map[string]interface{}{}, nil
 	}
 	var m map[string]interface{}
 	err = json.Unmarshal(content, &m)
@@ -46,5 +80,5 @@ func Save(req parser.Request, resp *http.Response) error {
 		return err
 	}
 
-	return ioutil.WriteFile(".hit.cache", f, 0)
+	return ioutil.WriteFile(cacheFilePath, f, 0600)
 }
