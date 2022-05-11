4ba63edf97e0c0233d5a9e2ad49f93567789f1a0
Harry Bagdi <harrybagdi@gmail.com>
2022-05-10 18:31:22 -0700

    test: add a basic test

--- pkg/test/core/core_test.go
+++ pkg/test/core/core_test.go
@@ -1,30 +1,47 @@
-package version
+package core
 
 import (
-	"bytes"
 	"context"
-	"os"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
-	"github.com/hbagdi/hit/pkg/cmd"
-	"github.com/hbagdi/hit/pkg/test/util"
+	"github.com/hbagdi/hit/pkg/cache"
+	"github.com/hbagdi/hit/pkg/executor"
+	"github.com/stretchr/testify/require"
 )
 
-func TestGET(t *testing.T) {
-	c := util.NewStdCapture()
-	defer c.Cleanup()
-	err := cmd.Run(context.Background(), "test-binary-name", "@get-headers")
-	if err != nil {
-		t.Errorf("expected err to be nil but got %v", err)
-	}
-	c.Stop()
-	out := c.Stdout()
+var c = cache.Get()
 
-	expected, err := os.ReadFile("../../completion/hit-completion.bash")
-	if err != nil {
-		t.Errorf("not expected err: %v", err)
-	}
-	if !bytes.Equal(expected, out) {
-		t.Errorf("unexpected output")
-	}
+func testExecutor(t *testing.T) *executor.Executor {
+	t.Helper()
+	e, err := executor.NewExecutor(&executor.Opts{Cache: c})
+	require.Nil(t, err)
+	return e
+}
+
+func TestGET(t *testing.T) {
+	e := testExecutor(t)
+	defer e.Close()
+
+	require.Nil(t, e.LoadFiles())
+	req, err := e.BuildRequest("get-headers", nil)
+	require.Nil(t, err)
+	require.NotNil(t, req)
+	require.Equal(t, "https://httpbin.org/headers",
+		req.HTTPRequest.URL.String())
+
+	res, err := e.Execute(context.Background(), req)
+	require.Nil(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, http.StatusOK, res.StatusCode)
+	defer res.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	require.Nil(t, err)
+	var m map[string]interface{}
+	require.Nil(t, json.Unmarshal(bodyBytes, &m))
+	h, ok := m["headers"].(map[string]interface{})
+	require.True(t, ok)
+	require.Equal(t, "bar", h["Foo"])
 }
