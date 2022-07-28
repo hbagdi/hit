5d86073a4cde86a9ba42b95d1422e37544de85b4
Harry Bagdi <harrybagdi@gmail.com>
2022-07-27 19:57:31 -0700

    fix: deferring os.Exit() never panics
    If there is a panic then it is eaten up by defer calls since os.Exit()
    is called.

--- main.go
+++ main.go
@@ -11,13 +11,9 @@ import (
 )
 
 func main() {
-	exitCode := 0
-	defer func() { os.Exit(exitCode) }()
-	ctx := context.Background()
-	ctx, done := signal.NotifyContext(ctx, syscall.SIGTERM, syscall.SIGINT)
-	defer done()
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 	if err := cmd.Run(ctx, os.Args...); err != nil {
-		exitCode = 1
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
