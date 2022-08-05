ce6431d4bc460c6889f2c3aa0d5838803e773fa7
Harry Bagdi <harrybagdi@gmail.com>
2022-08-04 20:03:41 -0700

    wip

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/participle/v2/lexer"
 	"github.com/alecthomas/repr"
-	"log"
-	"net/http"
-	"os"
 )
 
 var hitLexer = lexer.MustSimple([]lexer.SimpleRule{
@@ -15,8 +11,9 @@ var hitLexer = lexer.MustSimple([]lexer.SimpleRule{
 	{"Ident", `[a-zA-Z_]\w*`},
 	{"Path", `/.*`},
 	{"Punct", `[[!@#~$%^&*()+_={}\|:;"'<,>.?/]|]`},
-	{"whitespace", `[\s]+`},
-	{"Marker", `[\-]{3}`},
+	{"Whitespace", `[\t]+`},
+	{"NL", `\n`},
+	{"SingleSpace", `[ ]{1}`},
 })
 
 type File struct {
@@ -24,36 +21,37 @@ type File struct {
 }
 
 type Requests struct {
-	Id           string `"---" "@"@Ident`
-	Method       string `@Ident?`
-	Path         string `@Path `
-	BodyEncoding string `("~"@Ident)?`
-	Body         string `@!Marker* Marker`
+	Id     string `"@"@Ident`
+	NL     string `| @NL`
+	Method string `@Ident`
+	Space  string `@SingleSpace`
+	Path   string `@Path`
+	NL1    string `@NL`
+	Start  string `"~"`
+	// Body includes new lines
+	Body string `(@Ident@NL@SingleSpace)*`
+	End  string `"~"`
+	NL2  string `| @NL+`
 }
 
 var parser = participle.MustBuild(&File{},
 	participle.Lexer(hitLexer),
 )
 
+var input = `# comment
+@c1
+GET /foo
+~
+foobar
+body
+~
+`
+
 func main() {
 	hitFile := &File{}
-	err := parser.Parse("", os.Stdin, hitFile)
+	err := parser.ParseString("", input, hitFile)
 	if err != nil {
 		panic(err)
 	}
 	repr.Println(hitFile, repr.Indent("  "), repr.OmitEmpty(true))
-	for _, req := range hitFile.Requests {
-		fmt.Println(req.Body)
-		if req.Id == "c1" {
-			req, err := http.NewRequest(req.Method, "http://httpbin.org"+req.Path, nil)
-			if err != nil {
-				log.Println(err)
-			}
-			res, err := http.DefaultClient.Do(req)
-			if err != nil {
-				log.Println(err)
-			}
-			repr.Println(res.StatusCode)
-		}
-	}
 }
